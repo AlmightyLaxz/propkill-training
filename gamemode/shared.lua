706a8a16a869582c20d6d223d369bbaf5e654afe
AlmightyLaxz <almightylaxz@gmail.com>
2015-12-09 17:17:46 +1030

    updated main lua files

---
+++ gamemode/shared.lua
@@ -0,0 +1,10 @@
+GM.Name = "Propkill Training"
+GM.Author = "Almighty Laxz and Iced Coffee"
+GM.Email = "N/A"
+GM.Website = "N/A"
+
+DeriveGamemode("sandbox")
+
+function GM:Initialize()
+	-- Do stuff
+end
